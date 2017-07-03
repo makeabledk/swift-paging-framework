ef3412c6374873152a9458ab2ef5f9ca2d912ba7
Andreas <andreas@makeable.dk>
2017-07-03 14:03:13 +0200

    Should work now.

--- PagingMediatorFramework/PagingMediatorFramework/PagingMediatorFramework.swift
+++ PagingMediatorFramework/PagingMediatorFramework/PagingMediatorFramework.swift
@@ -10,8 +10,12 @@ import UIKit
 /**
  This class represents an Error as a result to a PagingMediator.getResults() call, it is returned whenever something went wrong in the call.
  */
-struct PagingMediatorError {
+public struct PagingMediatorError {
     var errorMessage: String
+    
+    public init(errorMessage: String) {
+        self.errorMessage = errorMessage
+    }
 }
 
 /**
@@ -23,7 +27,7 @@ struct PagingMediatorError {
 /// **FuncParameters:** typically either a tuple/array of parameters, or a single parameter for the pagedFunc
 ///
 /// **ReturnType:** a list of results. This list can be of any type.
-class PagingMediator<FuncParameters, ReturnType: Collection> {
+public class PagingMediator<FuncParameters, ReturnType: Collection> {
     
     private var isLoadingMoreData: Bool = false
     
@@ -42,7 +46,7 @@ class PagingMediator<FuncParameters, ReturnType: Collection> {
     /// - Parameters:
     ///   - pageLimit: The supplied pageLimit should always be greater than or equal to the number of cells that can be shown in the tableView on "one" screen. So if 10 cells can fit on screen, the pageLimit should be >= 10.
     ///   - pagedFunc: This function is the network call that supports Paging. This is the function that will be wrapped in this Mediator Class.
-    init(pageLimit: Int, pagedFunc: @escaping (_ params: FuncParameters, _ page: Int, _ limit: Int, _ completion: @escaping (_ result: ReturnType?, _ totalCount: Int?, _ error: PagingMediatorError?) -> Void) -> ()) {
+    public init(pageLimit: Int, pagedFunc: @escaping (_ params: FuncParameters, _ page: Int, _ limit: Int, _ completion: @escaping (_ result: ReturnType?, _ totalCount: Int?, _ error: PagingMediatorError?) -> Void) -> ()) {
         self.pageLimit = pageLimit
         self.page = 1
         self.totalPages = 0
@@ -52,7 +56,7 @@ class PagingMediator<FuncParameters, ReturnType: Collection> {
     /**
      This function is called whenever results needs to be fetched for the current mediator. If there is an error, the results and the totalCount will be nil, and there will be returned a PagingError.Error reference.
      */
-    func getResults(params: FuncParameters, completion: @escaping (_ result: ReturnType?, _ error: PagingMediatorError?) -> Void) {
+    public func getResults(params: FuncParameters, completion: @escaping (_ result: ReturnType?, _ error: PagingMediatorError?) -> Void) {
         self.isLoadingMoreData = true // Set to true, because pagedFunc is called next
         
         self.pagedFunc(params, self.page, self.pageLimit) { (results, totalCount, error) in
@@ -76,13 +80,13 @@ class PagingMediator<FuncParameters, ReturnType: Collection> {
         }
     }
     
-    fileprivate var moreResults: Bool {
+    private var moreResults: Bool {
         get {
             return self.page <= self.totalPages
         }
     }
     
-    func reset() {
+    public func reset() {
         self.page = 1
         self.totalPages = 0
         self.isLoadingMoreData = false
@@ -91,7 +95,7 @@ class PagingMediator<FuncParameters, ReturnType: Collection> {
     /**
      This function needs to be called in a tableView's cellForRowAt dataSource method. It returns true if the PagingMediator has more objects to load, and if the tableView has reached a point in the scrolling where the new data should be fetched, this is based on the loadOffset parameter. If the loadOffset is 5, then this function returns true if we have reached the numberOfRows-loadOffset row, and therefor should load the next data. Default value for loadOffset is 1: which loads when the last row has been reached.
      */
-    func tableViewShouldLoadMoreData(loadOffset: Int? = nil, indexPath: IndexPath, tableView: UITableView) -> Bool {
+    public func tableViewShouldLoadMoreData(loadOffset: Int? = nil, indexPath: IndexPath, tableView: UITableView) -> Bool {
         var result: Bool = false
         
         var rowNumber: Int = 1
